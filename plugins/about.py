d3badf2f19c275d14654d79dd2cbdd784c37b663
animationcreator38 <147336591+animationcreator38@users.noreply.github.com>
2023-11-03 23:40:50 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','6624704482:AAFvMZbokVy8CSKa-mGnFLeI-TlyfXPeGVA')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
